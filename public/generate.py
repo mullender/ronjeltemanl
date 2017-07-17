d18ab001d9ce875aac85774e42e49799e72e80b2
Matthijs Mullender <mmullender@groupon.com>
2017-07-17 13:30:07 -0700

    initial commit

---
+++ public/generate.py
@@ -0,0 +1,12 @@
+import json
+from string import Template
+
+with open('content.json', 'r') as f:
+    decoder = json.JSONDecoder(object_hook=None, parse_float=None, parse_int=None, parse_constant=None, strict=False)
+    j = decoder.decode(f.read())
+
+with open('index.template.html', 'r') as f:
+    s = Template(f.read())
+    for page in j:
+        with open(page['url'], 'w') as out:
+            out.write(s.substitute(page))
